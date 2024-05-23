cfe2c54e4f0ec3e0e1e076dbf2a9f45180ad9c34
SArthurX <sxa05z@gmail.com>
2024-05-23 08:33:09 +0800

    app init

---
+++ lib/select.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class select_Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page B'),
+      ),
+      body: Center(
+        child: Text('這是 Page B'),
+      ),
+    );
+  }
+}
